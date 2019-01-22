1824adeb1ca8e7d5c97efabce9737d99ab66f3b7
Danielle <dmonahan@udel.edu>
2019-01-22 11:32:06 -0500

    finish front-end user authentication

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,42 @@
+import * as ApiUtil from '../util/session_api_util';
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_ERRORS';
+
+export const login = (user) => dispatch => {
+    return ApiUtil.login(user)
+        .then(
+            currentUser => dispatch(receiveCurrentUser(currentUser)),
+            errors => dispatch(receiveErrors(errors))
+            );
+};
+
+export const logout = () => dispatch => {
+    return ApiUtil.logout()
+        .then(
+            user => dispatch(logoutCurrentUser())
+            );
+};
+
+export const signup = (user) => dispatch => {
+    return ApiUtil.signup(user)
+        .then(
+            user => dispatch(receiveCurrentUser(user)), 
+            errors => dispatch(receiveErrors(errors.responseJSON))
+            );
+};
+
+const receiveCurrentUser = (currentUser) => ({
+    type: RECEIVE_CURRENT_USER,
+    currentUser,
+});
+
+const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER,
+});
+
+const receiveErrors = (errors) => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors,
+});
+
--- frontend/pixtrest.jsx
+++ frontend/pixtrest.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore()
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Pixtrest</h1>, root);
 });
 
-import {signup,login,logout} from '../frontend/util/session_api_util'
-window.signup = signup;
-window.login = login;
-window.logout = logout;
+
+// testing only
+// import {signup,login,logout} from '../frontend/util/session_api_util'
+// window.signup = signup;
+// window.login = login;
+// window.logout = logout;
