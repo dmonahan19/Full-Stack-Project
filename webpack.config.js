61bea0cf27f5b66ca934544814873e1b6f801a09
Danielle <dmonahan@udel.edu>
2019-01-22 17:47:31 -0500

    change webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = {
     context: __dirname,
-    entry: './frontend/pixtrest.jsx',
+    entry: {
+        path: path.resolve(__dirname, 'frontend'),
+        filename: 'pixterest.jsx',
+    },
     output: {
         path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
         filename: 'bundle.js'
