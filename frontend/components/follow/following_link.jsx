264b1f9fe2017f6eced75bd5394d7225c342d20a
Danielle <dmonahan@udel.edu>
2019-04-09 20:33:22 -0400

    fix followlink when no following feed

--- frontend/components/follow/following_link.jsx
+++ frontend/components/follow/following_link.jsx
@@ -17,37 +17,42 @@ class FollowingLink extends React.Component {
         let col3 = [];
         let col4 = [];
         let i = 0;
-        this.props.pins.map((pin, i) => {
-                if (i % 4 === 0) {
-                    col1.push(<FollowingPinItem
-                        key={i}
-                        pin={pin}
-                        openModal={this.props.openModal}
-                    />);
-                }
-                if (i % 4 === 1) {
-                    col2.push(<FollowingPinItem
-                        key={i}
-                        pin={pin}
-                        openModal={this.props.openModal}
-                    />);
-                }
-                if (i % 4 === 2) {
-                    col3.push(<FollowingPinItem
-                        key={i}
-                        pin={pin}
-                        openModal={this.props.openModal}
-                    />);
-                }
-                if (i % 4 === 3) {
-                    col4.push(<FollowingPinItem
-                        key={i}
-                        pin={pin}
-                        openModal={this.props.openModal}
-                    />);
-                }
-                i++
-        });
+        if (this.props.user.following_userIds.length > 0){
+            this.props.pins.map((pin, i) => {
+                    if (i % 4 === 0) {
+                        col1.push(<FollowingPinItem
+                            key={i}
+                            pin={pin}
+                            openModal={this.props.openModal}
+                        />);
+                    }
+                    if (i % 4 === 1) {
+                        col2.push(<FollowingPinItem
+                            key={i}
+                            pin={pin}
+                            openModal={this.props.openModal}
+                        />);
+                    }
+                    if (i % 4 === 2) {
+                        col3.push(<FollowingPinItem
+                            key={i}
+                            pin={pin}
+                            openModal={this.props.openModal}
+                        />);
+                    }
+                    if (i % 4 === 3) {
+                        col4.push(<FollowingPinItem
+                            key={i}
+                            pin={pin}
+                            openModal={this.props.openModal}
+                        />);
+                    }
+                    i++
+            });
+        }
+        else{
+            col1.push(<h1 key={"key1"} className='no-followers'>Your Following feed is empty!</h1>)
+        }
         
         return (
             <>
